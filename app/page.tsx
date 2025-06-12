c469ca78a5b8771d643eeb4faccdf44f73cf8758
show kawazoe <show753@icloud.com>
2025-06-12 09:09:30 +0900

    get post from aws

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
-import Image from "next/image";
+import { API } from 'aws-amplify';
+import { listPosts } from '../src/graphql/queries';
 
-export default function Home() {
-  return <h1 className="text-6xl font-bold underline">Hello!!</h1>
+export default async function Home() {
+  const postData: any = await API.graphql({ query: listPosts });
+  const posts = postData.data?.listPosts?.items || [];
+
+  return (
+    <div>
+      <h1 className="text-6xl font-bold underline">My Posts</h1>
+      {posts.length > 0 ? (
+        <ul>
+          {posts.map((post: any) => (
+            <li key={post.id}>
+              {post.title} - {post.content}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No posts available</p>
+      )}
+    </div>
+  );
 }
